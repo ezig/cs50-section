c2975ef40e3c1c3ed3112e4c0c43df7745c28dc6
ezigmond1 <ezigmond@college.harvard.edu>
2015-09-20 01:28:49 +0000

    Added scope example

---
+++ week3/scope.c
@@ -0,0 +1,45 @@
+/*
+ * args0.c
+ * Ezra Zigmond
+ * CS50 week3
+ *
+ * Goal: Understand scope of variables
+ */
+
+#include <stdio.h>
+
+void scope1(void);
+void scope2(int a);
+void scope3(void);
+
+int main(void)
+{
+    scope1();
+    scope2(27);
+    scope3();
+}
+
+void scope1(void)
+{
+    {
+        int a = 4;
+        printf("%d\n", a); // what will this print?
+    }
+
+    {
+        int a = 5;
+        printf("%d\n", a); // how about this?
+    }
+    
+    printf("%d\n", a); // this one?
+}
+
+void scope2(int a)
+{
+    printf("%d\n", a); // how about now
+}
+
+void scope3(void)
+{
+    printf("%d\n", a); // ??????
+}
